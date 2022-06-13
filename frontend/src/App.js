e81257b40b117ca8ecd54d32f441235158490e33
Godbell <12191579@inha.edu>
2022-06-14 08:34:32 +0900

    manage react scripts indent to tab

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [message, setMessage] = useState('');
+	const [message, setMessage] = useState('');
 
-  useEffect(() =>
-  {
-    fetch('/hello')
-    .then(res => res.text())
-    .then(text => {setMessage(text); console.log(text)});
-  });
+	useEffect(() => {
+		fetch('/hello')
+			.then(res => res.text())
+			.then(text => { setMessage(text); console.log(text) });
+	});
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-	  {message}
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					{message}
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
